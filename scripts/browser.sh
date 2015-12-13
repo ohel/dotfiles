ff4d126f062494d1f6c6b0a2c7e6a15f83f37deb
ohel <ohel@users.noreply.github.com>
2015-12-13 13:51:48 +0200

    Added browser script.

---
+++ scripts/browser.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if test "X$1" == "newwindow"
+then
+    chromium-browser
+    exit
+elif [ "$#" -eq 1 ];
+then
+    chromium-browser "$1"
+elif test "$(ps -ef | grep chromium-browser | grep -v grep)X" == "X"
+then
+    chromium-browser
+else
+    chromium-browser about:blank
+fi
+wmctrl -i -a $(wmctrl -l | grep -e "Chromium" | tail -n 1 | cut -f 1 -d ' ')
