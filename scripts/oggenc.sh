0e5b8c79fee5a01aac60407beac2d59b6efbb594
ohel <ohel@users.noreply.github.com>
2015-12-15 02:07:01 +0200

    Add overwrite switch.

--- scripts/oggenc.sh
+++ scripts/oggenc.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 # Encode all flac files in subdirectories to ogg vorbis in batches of $batchsize. This is far from perfect parallelization, but good enough.
 
+overwrite=0
 batchsize=4
+oggdir=ogg
 
-mkdir -p ogg
+mkdir -p $oggdir
 
 flacs=$(find ./ -iname "*.flac")
 batchindex=0
 for flacfile in $flacs
 do
-    echo Encoding $flacfile...
     filebase=$(echo $flacfile | sed s/\.flac$//)
-    batchindex=$((($batchindex + 1) % 4))
 
-    oggenc -Q -q 7 -o ogg/$filebase.ogg $filebase.flac &
+    if [ ! -e $oggdir/$filebase.ogg ] || [ $overwrite -eq 1 ]
+    then
+        echo Encoding $flacfile...
 
-    if [ $batchindex -eq 0 ]; then wait; fi
+        batchindex=$((($batchindex + 1) % 4))
+        oggenc -Q -q 7 -o $oggdir/$filebase.ogg $filebase.flac &
+
+        if [ $batchindex -eq 0 ]; then wait; fi
+    else
+        echo "Skipped existing file: $flacfile.ogg"
+    fi
 done
 wait
 echo "Done."
