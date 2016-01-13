53fe4379e8e57c3f1cf102947f931da5511c62b0
ohel <ohel@users.noreply.github.com>
2016-01-13 18:51:49 +0200

    Fix for button jam.

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-if test "X$1" == "newwindow"
+focuswin=$(wmctrl -l | grep -e "Chromium" | tail -n 1 | cut -f 1 -d ' ')
+if test "X$focuswin" != "X"
+then
+    wmctrl -i -a $focuswin
+fi
+
+if test "X$1" = "Xnewwindow"
 then
     chromium-browser
     exit
 elif [ "$#" -eq 1 ];
 then
     chromium-browser "$1"
-elif test "$(ps -ef | grep chromium-browser | grep -v grep)X" == "X"
+elif test "$(ps -ef | grep chromium-browser | grep -v grep)X" = "X"
 then
     chromium-browser
+else
+    #chromium-browser about:blank
+    # Assuming Win+w is the shortcut, keyup w first.
+    xdotool keyup w && xdotool keyup Mod4 && xdotool key Ctrl+t
 fi
-wmctrl -i -a $(wmctrl -l | grep -e "Chromium" | tail -n 1 | cut -f 1 -d ' ')
