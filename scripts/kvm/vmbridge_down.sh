dc0bbc2ab474d8fc59b7b831a0b29a42d1305ab3
Olli Helin <olli.helin@gofore.com>
2016-03-22 18:41:01 +0200

    Remove constant string.

--- scripts/kvm/vmbridge_down.sh
+++ scripts/kvm/vmbridge_down.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-ip=$(ifconfig | grep -A 1 wlp1s0 | tail -n 1 | cut -f 2 -d ":" | cut -f 1 -d " " | grep "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$")
+adapter=wlp1s0
+ip=$(ifconfig | grep -A 1 $adapter | tail -n 1 | cut -f 2 -d ":" | cut -f 1 -d " " | grep "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$")
 if test "X$ip" == "X"
 then
     ip=127.0.0.1
     echo "Using localhost as source NAT IP"
 fi
 
-sysctl -q -e -w net.ipv4.conf.wlp1s0.forwarding=0
-iptables -t nat -D POSTROUTING -s 10.0.1.0/24 -o wlp1s0 -m iprange ! --dst-range 10.0.0.0-10.255.255.255 -m iprange ! --dst-range 127.0.0.1 -j SNAT --to-source $ip
+sysctl -q -e -w net.ipv4.conf.$adapter.forwarding=0
+iptables -t nat -D POSTROUTING -s 10.0.1.0/24 -o $adapter -m iprange ! --dst-range 10.0.0.0-10.255.255.255 -m iprange ! --dst-range 127.0.0.1 -j SNAT --to-source $ip
 
 sysctl -q -e -w net.ipv4.conf.vmbridge.forwarding=0
 iptables -t nat -D POSTROUTING -s 10.0.1.1/32 -o vmbridge -m iprange ! --dst-range 10.0.0.0-10.255.255.255 -m iprange ! --dst-range 127.0.0.1 -j SNAT --to-source $ip
