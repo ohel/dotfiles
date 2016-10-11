2b8f5fef062fe6a33682fcf4c04583ef85eeaef9
ohel <ohel@users.noreply.github.com>
2016-10-11 22:33:10 +0300

    Script.

---
+++ scripts/youtube-dl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Download YouTube videos using youtube-dl in a well compressed, good-quality format.
+
+audio_fid=171 # vorbis@128k
+video_fid=247 # vp9, 25fps, 720p
+
+if [ "$#" -ne 1 ]
+then
+  exit
+fi
+
+destination=$(youtube-dl -f $audio_fid -o "%(title)s" $1 | grep Destination | cut -f 2 -d ':' | sed -e 's/^[[:space:]]*//')
+youtube-dl -f $video_fid -o "$destination.video" $1
+
+ffmpeg -i "$destination.video" -i "$destination" -c copy -map 0:v:0 -map 1:a:0 "$destination.mkv"
+rm "$destination"
+rm "$destination.video"
