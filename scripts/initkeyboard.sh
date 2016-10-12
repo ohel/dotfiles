5a62a4c5fba099960c3a382e1e81f19132c1e19e
ohel <ohel@users.noreply.github.com>
2016-10-12 18:43:58 +0300

    Tweaks.

--- scripts/initkeyboard.sh
+++ scripts/initkeyboard.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Sometimes at least the Microsoft Natural Ergonomic 4000 keyboard acts weirdly or hangs up.
+# Reconnecting it physically works, but I have to set everything anew. This script solves that.
+
 killall -q xbindkeys
 setxkbmap fi
 xset r rate 200 45
