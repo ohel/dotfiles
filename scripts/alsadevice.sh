57e3836a272adbbf1825a65387fb63d4d761da3d
ohel <ohel@users.noreply.github.com>
2016-10-15 18:31:25 +0300

    Cleanup.

--- scripts/alsadevice.sh
+++ scripts/alsadevice.sh
@@ -6,9 +6,7 @@ if [ "$#" = 0 ]; then
  echo "hda   (hda)"
  echo "hdmi  (hdmi_out)"
  echo "julia (julia)"
- echo "jv    (julia_vol)"
  echo "eq    (hda_eq)"
- echo "file  (file_out)"
  echo "null"
  echo ""
 else
@@ -24,14 +22,8 @@ else
  elif [ "$1" = "julia" ]; then
   pcm="julia"
   ctl="julia_digital_hw"
- elif [ "$1" = "jv" ]; then
-  pcm="julia_vol"
-  ctl="julia_digital_hw"
  elif [ "$1" = "eq" ]; then
   pcm="hda_eq"
-  ctl="hda_hw"
- elif [ "$1" = "file" ]; then
-  pcm="file_out"
   ctl="hda_hw"
  elif [ "$1" = "null" ]; then
   pcm="null"
