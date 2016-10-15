43ecf8f7d20ddc919d8d8b99a85d8ac12af28abf
ohel <ohel@users.noreply.github.com>
2016-10-15 23:49:03 +0300

    Support ffmpeg audio encoding.

--- scripts/wav2aac4video.sh
+++ scripts/wav2aac4video.sh
@@ -5,10 +5,16 @@
 # using AAC and mux the results in a single .mp4 file.
 
 # Audio is resampled to 44.1 kHz and bitrate is given via an optional second parameter.
-# Needs ffmpeg, sox and fdkaac to work.
+# Needs ffmpeg with fdkaac libs, with sox (for normalizing) and fdkaac encoders optionally.
 
 input=$1
-bitrate=${2:-96000}
+quality=${2:-2}
+
+if [[ $quality -lt 1 || $quality -gt 5 ]]
+then
+    echo "Audio quality must be between 1 and 5, given as integer."
+    exit
+fi
 
 if test "X$input" == "X"
 then
@@ -25,12 +31,26 @@ tmpfile=/dev/shm/out
 
 echo Extracting audio...
 ffmpeg -loglevel fatal -i $input -c copy -map 0:a:0 $tmpfile.wav
-echo Normalizing audio...
-sox -q $tmpfile.wav -r 44.1k --norm=-3 $tmpfile.normalized.wav
+
+if test "X$(which sox 2>/dev/null)" != "X"
+then
+    echo Normalizing audio...
+    sox -q $tmpfile.wav -r 44.1k --norm=-3 $tmpfile.normalized.wav
+else
+    mv $tmpfile.wav $tmpfile.normalized.wav
+fi
+
 echo Compressing audio...
-fdkaac -S -b $bitrate $tmpfile.normalized.wav -o $tmpfile.m4a
+if test "X$(which fdkaac 2>/dev/null)" != "X"
+then
+    bitrate=$(echo 48000*$quality | bc)
+    fdkaac -S -b $bitrate $tmpfile.normalized.wav -o $tmpfile.m4a
+else
+    ffmpeg -loglevel fatal -i $tmpfile.normalized.wav -codec:a libfdk_aac -aq 0.0$quality -ar 44100 $tmpfile.m4a
+fi
+
 echo Muxing with video...
 ffmpeg -loglevel fatal -i $input -i $tmpfile.m4a -c copy -map 0:v:0 -map 1:a:0 $(basename -s .avi $input).mp4
-rm $tmpfile.normalized.wav $tmpfile.wav $tmpfile.m4a
+rm $tmpfile.normalized.wav $tmpfile.wav $tmpfile.m4a 2>/dev/null
 echo Done.
 
