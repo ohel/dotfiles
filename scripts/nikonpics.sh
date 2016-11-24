ec9503c9faf493c54f6909dcaf5d14db689da9db
ohel <ohel@users.noreply.github.com>
2016-11-24 03:26:13 +0200

    Add Nikon script.

---
+++ scripts/nikonpics.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Download pictures from Nikon Coolpix S7000 via MTP using gphoto2.
+
+folder="/store_00010001/DCIM/100NIKON"
+
+gphoto2 -f $folder -L
+echo
+echo "Press return to download all."
+read
+gphoto2 -f $folder -P
+echo
+echo "Press return to delete all."
+read
+gphoto2 -f $folder -D
