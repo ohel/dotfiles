fbcf8e6e3e6ee6be5ad219fbacc794389bab8a5f
ohel <ohel@users.noreply.github.com>
2016-12-01 01:00:52 +0200

    Add script, alias.

---
+++ scripts/zero_empty_space.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Zero empty space on a file system. Use another smaller file so that filling out the space doesn't result in hiccups.
+
+dd if=/dev/zero of=zero.small.file bs=1024 count=102400
+cat /dev/zero > zero.file
+sync
+rm zero.small.file
+rm zero.file
