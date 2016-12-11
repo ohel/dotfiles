dce4464b9de3d176c3cec206e7d6afb364a5c537
ohel <ohel@users.noreply.github.com>
2016-12-11 20:12:38 +0200

    Add thumbs script, modify index.

--- scripts/create_index.sh
+++ scripts/create_index.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
+# Create a simple index.html from directory contents up to one level of sub directories.
+# White spaces are not supported in file or directory names.
+# A dummy .htaccess file is written also.
 
-ROOT="subdir/"
-OUTPUT="index.html" 
+root=${1:-"."}
+index="index.html" 
 
-i=0
-echo "<UL>" > $OUTPUT
-for filepath in `find ./ -maxdepth 1 -mindepth 1 -type d| sort`; do
-  path=`basename "$filepath"`
-  echo "  <LI>$path</LI>" >> $OUTPUT
-  echo "  <UL>" >> $OUTPUT
-  for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort`; do
-    file=`basename "$i"`
-    echo "    <LI><a href=\"/$ROOT$path/$file\">$file</a></LI>" >> $OUTPUT
-  done
-  echo "  </UL>" >> $OUTPUT
+echo "<html lang=\"en\"><head><meta charset=\"utf-8\"></head><body>" > $index
+echo "<ul>" > $index
+for filename in `find ./ -maxdepth 1 -type f | sort`; do
+    item=`basename "$filename"`
+    if test "$item" != "index.html"; then
+        echo "  <li><a href=\"/$root/$item\">$item</a></li>" >> $index
+    fi
 done
-echo "</UL>" >> $OUTPUT
+echo "</ul><ul>" >> $index
+for filepath in `find ./ -maxdepth 1 -mindepth 1 -type d | sort`; do
+  path=`basename "$filepath"`
+  echo "  <li>$path</li>" >> $index
+  echo "  <ul>" >> $index
+  for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f | sort`; do
+    file=`basename "$i"`
+    echo "    <li><a href=\"/$root/$path/$file\">$file</a></li>" >> $index
+  done
+  echo "  </ul>" >> $index
+done
+echo "</ul>" >> $index
+echo "</body></html>" >> $index
+
+echo "RewriteEngine off" > .htaccess
+echo "DirectoryIndex $index" >> .htaccess
+
+chmod a+r .htaccess $index
+
+echo "Created index.html and .htaccess."
+echo "Remember to chmod -R a+r the files and directories."
---
+++ scripts/create_thumbs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Create thumbnails of given size ($1) of given pictures. For use in Markdown documents (blogs) etc.
+
+if [[ "$#" -lt 1 ]] || ! [[ $1 =~ ^[0-9]+$ ]]
+then
+    echo "Give the maximum thumbnail size as a first parameter (\$1 x \$1 pixels)."
+    echo "A nice example size for blog articles is 450."
+    exit
+fi
+size=$1
+
+if [ "$#" -lt 2 ]
+then
+    echo "Give the names of the pictures as parameters after the size parameter."
+fi
+
+shift
+
+while [ -n "$1" ]
+do
+
+    filename_thumb=thumb_"${1%.*}".jpg
+
+    if [ -e "$filename_thumb" ]
+    then
+        echo "Error: the picture $filename_thumb exists. Move it away."
+        shift
+        continue
+    fi
+
+    convert "$1" -resize "$size"x"$size" -strip -quality 75 "$filename_thumb"
+
+    echo "Created thumbnail: $filename_thumb"
+    echo "Copy-paste for the blog:"
+    echo "[![Description.]($filename_thumb)]($1)"
+    echo ""
+
+    shift
+
+done
