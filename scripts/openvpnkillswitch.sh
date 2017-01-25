b43c18a216e9b16ec6061ff2156e170874d686b5
ohel <ohel@users.noreply.github.com>
2017-01-25 03:11:34 +0200

    Add OpenVPN kill switch.

---
+++ scripts/openvpnkillswitch.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+# Establish an OpenVPN connection using $1 as config.
+# Creates an iptables filter working as a VPN kill switch.
+# VPN server IP is deduced after succesful connection.
+
+# For now we need to know beforehand how many IP routes the VPN
+# connection is expected to create when initializing.
+routes_to_create=5
+
+if [ "$#" == 0 ]
+then
+    echo "You must give the OpenVPN config file as parameter."
+    exit
+fi
+
+for physical_device in $(ls -l /sys/class/net | grep devices\/pci | grep -o " [^ ]* ->" | cut -f 2 -d ' ')
+do
+    ip=$(ip addr show $physical_device | grep -o "inet [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" | cut -f 2 -d ' ')
+    if test "X$ip" != "X"
+    then
+        IF=$physical_device
+        break
+    fi
+done
+
+current_routes=$(ip route show | grep $IF | cut -f 1 -d ' ')
+num_routes=$(expr $(ip route show | wc -l) + $routes_to_create)
+
+openvpn $1 &
+
+# Wait till VPN routes are set up.
+while [ $(ip route show | wc -l) -ne $num_routes ]
+do
+    sleep 0.5
+done
+
+new_routes=$(ip route show | grep $IF | cut -f 1 -d ' ')
+vpn_ip=""
+for route in $new_routes
+do
+    candidate=$route
+    for counterpart in $current_routes
+    do
+        if test "$route" == "$counterpart"
+        then
+            candidate=""
+        fi
+    done
+    vpn_ip=$candidate
+done
+if test "X$vpn_ip" == "X"
+then
+    echo "ERROR: VPN IP not found from routes."
+    exit
+fi
+
+iptables -F
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -i tun+ -j ACCEPT
+iptables -A OUTPUT -o tun+ -j ACCEPT
+iptables -A INPUT -p udp --sport 1194 -s $vpn_ip -j ACCEPT
+iptables -A OUTPUT -p udp --dport 1194 -d $vpn_ip -j ACCEPT
+echo -e "\n\nCreated iptables rules.\n\n"
+
+function killvpn() {
+    echo -e "\n\nKilling VPN connection.\n\n"
+}
+
+while [ 1 ]
+do
+    trap killvpn 2
+    wait
+    echo -e "\n\nVPN connection lost. Reconnecting in 3 seconds. To cancel, press c.\n\n"
+    read -s -N 1 -t 3 cancel
+    trap 2
+
+    if test "X$cancel" == "Xc"
+    then
+        break
+    fi
+
+    echo -e "\n\nAllowing DNS traffic outside VPN.\n\n"
+    iptables -A INPUT -p udp --sport 53 -j ACCEPT
+    iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+
+    num_routes=$(expr $(ip route show | wc -l) + $routes_to_create)
+    openvpn $1 &
+    while [ $(ip route show | wc -l) -ne $num_routes ]
+    do
+        sleep 0.5
+    done
+
+    iptables -D INPUT -p udp --sport 53 -j ACCEPT
+    iptables -D OUTPUT -p udp --dport 53 -j ACCEPT
+    echo -e "\n\nDNS traffic disabled outside VPN.\n\n"
+done
+
+iptables -F
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+echo -e "\n\nFlushed iptables rules.\n\n"
