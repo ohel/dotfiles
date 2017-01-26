bee47a344d76d286a7490ec7e30f4df67f761f87
ohel <ohel@users.noreply.github.com>
2017-01-26 02:17:47 +0200

    Fix + add shortcuts.

--- scripts/wifiscan.sh
+++ scripts/wifiscan.sh
@@ -4,5 +4,5 @@ sudo iwlist wlp1s0 scan | grep ESSID | cut -f 2 -d ":" | sed 's/^"\(.*\)"$/\1/'
 if [ "$#" -gt 0 ] && [ "$1" = "pause" ]; then
     echo
     echo "Press return to continue."
-    read
+    read temp
 fi
