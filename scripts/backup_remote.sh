7407821de140d5af88db99f7a90ca7a5102f17a3
ohel <ohel@users.noreply.github.com>
2017-02-08 20:01:05 +0200

    Script updates.

--- scripts/backup_remote.sh
+++ scripts/backup_remote.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-# A generic echoing backup script to rsync something to a remote location.
-# There are three mandatory parameters, the rest are optional rsync excludes.
+# A generic echoing backup script to rsync something to or from a remote location.
+# There are four mandatory parameters, the rest are optional rsync excludes.
+# The parameters are:
+# $1: mode (normal, normaldry, reverse, reverse)
+# $2: localdir (the directory to back up)
+# $3: server (SSH)
+# $4: target (directory in server under $remotedir)
+# The mode parameter may be postfixed with nowait (e.g. normalnowait) to skip the countdown.
 
-remotedir="~/backups"
+remotedir="~/backups" # The target directory should exist in this directory on the server.
+countdown=3 # Wait this many seconds before actually starting the operation to prevent accidents.
 
-if [ "$#" -lt 3 ]; then
+if [ "$#" -lt 4 ]; then
     echo "Required parameters missing."
     exit
 fi
 
-sourcedir=$1
-server=$2
-target=$3
-shift; shift; shift
+mode=$1
+localdir=$2
+server=$3
+target=$4
+shift; shift; shift; shift
 excludelist=("$@")
 
 excludeparams=""
@@ -21,6 +29,30 @@ do
     excludeparams="$excludeparams --exclude=$excludeitem"
 done
 
-echo "Backing up $target..."
-rsync -avzu --delete $excludeparams $sourcedir $server:$remotedir/$target
+if test "X$(echo $mode | grep nowait\$)" != "X"
+then
+    countdown=0
+    mode=$(echo $mode | sed s/nowait\$//)
+fi
 
+if test "$mode" == "normal"
+then
+    echo "Starting \"$target\" backup to remote in $countdown seconds."
+    sleep $countdown
+    rsync -avzu --delete $excludeparams $localdir $server:$remotedir/$target
+elif test "$mode" == "normaldry"
+then
+    echo "Dry run backup \"$target\" to remote."
+    rsync -avzun --delete $excludeparams $localdir $server:$remotedir/$target
+elif test "$mode" == "reverse"
+then
+    echo "Starting \"$target\" synchronizing from remote in $countdown seconds."
+    sleep $countdown
+    rsync -avzu --delete $excludeparams $server:$remotedir/$target $localdir
+elif test "$mode" == "reversedry"
+then
+    echo "Dry run synchronize \"$target\" from remote."
+    rsync -avzun --delete $excludeparams $server:$remotedir/$target $localdir
+else
+    echo "Unknown backup mode: $mode. Aborted."
+fi
--- scripts/hwinfo.sh
+++ scripts/hwinfo.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 # Display some info about CPU and GPU (AMD).
 
-echo
 echo CPU core frequencies:
 cat /proc/cpuinfo | grep MHz
 echo
 echo CPU core temperatures:
 sensors | grep -A 4 Physical
-if test "$(uname -r | grep rt)" != "$(uname -r)"
+echo
+echo Graphics card:
+if test "X$(which amdconfig 2>/dev/null)" != "X"
 then
-	echo
-	echo Graphics card:
 	amdconfig --odgc | grep -B 1 Current
 	amdconfig --odgc | grep load
 	amdconfig --odgt | grep Sensor
+else
+    sensors | grep -A 2 radeon | tail -n 1 | cut -f 2 -d '+' | cut -f 1 -d ' '
 fi
-
+echo
