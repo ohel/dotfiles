f515b96bfe6d6209339bc98aafe58a34fb38a94d
ohel <ohel@users.noreply.github.com>
2017-07-25 00:22:28 +0300

    Tweaks. Remove duplicate.

--- scripts/alert.sh
+++ scripts/alert.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Play an alert sound if there is a file called alert.set in user's home directory.
+# May be used e.g. with an email client which generates the file on certain conditions.
+# Use crontab to schedule the alert checking.
 
 wav=${1:-~/.local/share/misc/alert.wav}
 alsadev=${2:-julia_aout}
--- scripts/wifiscan.sh
+++ scripts/wifiscan.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # Scan and list available wifi network ESSIDs.
-sudo iwlist wlp1s0 scan | grep ESSID | cut -f 2 -d ":" | sed 's/^"\(.*\)"$/\1/'
+interface=$(cat /proc/net/wireless | tail -n +3 | head -n 1 | cut -f 1 -d ':')
+if test "X$interface" = "X"
+then
+    echo No wifi interface found.
+    exit
+fi
+
+sudo iwlist $interface scan | grep ESSID | cut -f 2 -d ":" | sed 's/^"\(.*\)"$/\1/'
 if [ "$#" -gt 0 ] && [ "$1" = "pause" ]; then
     echo
     echo "Press return to continue."
