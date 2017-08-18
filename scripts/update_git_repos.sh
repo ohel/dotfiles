9aacfe647ff90a8bcddae780a803f788d3cefdf2
ohel <ohel@users.noreply.github.com>
2017-08-18 14:52:35 +0300

    Better git update script.

---
+++ scripts/update_git_repos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Updates all git repositories inside current directory.
+# Useful e.g. when keeping local bare repository backup mirrors of remote git repositories.
+
+cwd=$(pwd)
+git_dirs=$(find ./ -type d -name \*.git | sed "s/\/\.git\$//" | sed "s/^\.\///")
+for dir in ${git_dirs[@]}
+do
+    if test "X$1" = "Xpull"; then
+        echo Pulling repository: $dir
+        cd $cwd/$dir
+        git pull --ff-only -q &
+    else
+        echo Fetching repository: $dir
+        cd $cwd/$dir
+        git fetch --all -q -p &
+    fi
+done
+wait
+cd $cwd
