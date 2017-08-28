056c03eaeb2e815b22ac5ec781ea276aaba52f6c
ohel <ohel@users.noreply.github.com>
2017-08-28 22:52:31 +0300

    Add mtp automount script, remove obsolete.

---
+++ udev/scripts/mountmtp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Mount a device using simple-mtpfs.
+
+user=panther
+
+vid=$1 # vendor ID, from udev
+mid=$2 # model ID, from udev
+options=$3 # -u if unmount, otherwise empty
+
+if test "X$options" = "X-u"
+then
+    # Needs array intersection for multiple MTP devices to work nicely.
+    mountpoint=$(mount | grep simple-mtpfs | grep -o "/tmp/mtp-mount-..." | head -n 1)
+    su $user -c "fusermount -u $mountpoint"
+    rmdir $mountpoint
+else
+    devnum=$(simple-mtpfs -l 2>&1 | tail -n 1 | cut -f 1 -d ':')
+    mountpoint=$(mktemp -d /tmp/mtp-mount-XXX)
+    chown $user:$user $mountpoint
+    chmod 700 $mountpoint
+    su $user -c "simple-mtpfs --device $devnum $mountpoint"
+fi
