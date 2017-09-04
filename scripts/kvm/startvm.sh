e0d7425b85e60f22a0b769594720a1eab183879a
ohel <ohel@users.noreply.github.com>
2017-09-04 03:43:10 +0300

    Refactor.

--- scripts/kvm/startvm.sh
+++ scripts/kvm/startvm.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-audio=1
-vm_name="Xubuntu"
-img_name="xubuntu.img"
-net_id=10 # used as the static IP address in the guest, MAC address and VNC display
+# Start a Qemu-KVM virtual machine. Consider this script a template to copy per virtual machine.
+
+windows_guest=0
+boot_from_cd=0
+audio=1 # not supported on Windows guests
+auto_vnc=0
+img_name="${1:-"xubuntu.img"}"
+vm_name="${2:-"Xubuntu"}"
+net_id=${3:-10} # used as the last number of static IP address of the guest, MAC address and VNC display
+
 vm_bridge=vmbridge
-#cdrom_image="/opt/virtualmachines/image.iso"
-#vncviewer="gvncviewer localhost:$net_id"
+cdrom_image="image.iso"
+
+if test "X$(which gvncviewer)" != "X"
+then
+    vncviewer="gvncviewer localhost:$net_id"
+elif test "X$(which vncviewer)" != "X"
+then
+    vncviewer="vncviewer :$net_id"
+fi
 
 if test "X$(brctl show $vm_bridge 2>&1 | grep No)" != "X"
 then
@@ -32,7 +45,7 @@ fi
 dac=""
 adc=""
 soundhw=""
-if test "X$1" == "Xaudio" || test "$audio" == 1
+if test $audio = 1 && test $windows_guest = 0
 then
     modprobe snd-aloop
     dac="hw:Loopback,0,4" # loop_vm_dac_in
@@ -41,18 +54,25 @@ then
 fi
 
 bootstring="c"
-if test "X$cdrom_image" != "X"
+if test $boot_from_cd = 1
 then
     bootstring="d -cdrom $cdrom_image"
 fi
 
+if test $windows_guest = 1
+then
+    # Required for Windows guests.
+    cursor_fix="-usbdevice tablet"
+fi
+
 echo "Starting the virtual machine..."
-/bin/env QEMU_ALSA_DAC_DEV=$dac QEMU_ALSA_ADC_DEV=$adc qemu-system-x86_64 $soundhw -daemonize \
+env QEMU_ALSA_DAC_DEV=$dac QEMU_ALSA_ADC_DEV=$adc qemu-system-x86_64 $soundhw $cursor_fix \
+-daemonize \
 -name "$vm_name" \
 -boot $bootstring \
 -machine q35,accel=kvm \
 -cpu host \
--smp 2 \
+-smp cores=2,threads=1,sockets=1 \
 -m 4096 \
 -k fi \
 -display none \
@@ -60,21 +80,23 @@ echo "Starting the virtual machine..."
 -net tap,script="kvm_net_up.sh" \
 -drive file="$img_name",if=virtio,format=raw \
 -vnc :$net_id
-# -cpu core2duo: fixes most problems and some BSODs in Windows
+
+# Common workarounds and tweaks:
+# -cpu core2duo: fixes most problems and some BSODs in Windows, especially during setup
 # -no-kvm-irqchip: required if VM hangs during POST until VNC connection is established
-# -usbdevice tablet: required for Windows guests
+# -device usb-host,hostbus=1,hostport=1: check bus and port with lsusb -t to pass a single USB port through to client
+# -device usb-ehci,id=usb,bus=pcie.0,addr=0x4: needed if passing a USB port with device detached (addr seems arbitrary)
 
 sleep 2
 
-# The PID changes so cannot use last PID.
+# The PID changes so we cannot use last PID.
 pid=$(ps -ef | grep "qemu.*$vm_name" | grep -v grep | tr -s ' ' | cut -f 2 -d ' ')
 if test "X$pid" != "X"
 then
     renice +15 $pid
 fi
 
-if test "X$vncviewer" != "X"
+if test $auto_vnc = 1 && "X$vncviewer" != "X"
 then
     $vncviewer &
 fi
-
