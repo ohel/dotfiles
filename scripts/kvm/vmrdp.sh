24747b469451e4b5a4025fbae47ecec464e2f90c
ohel <ohel@users.noreply.github.com>
2017-09-07 03:49:44 +0300

    Audio support.

--- scripts/kvm/vmrdp.sh
+++ scripts/kvm/vmrdp.sh
@@ -1,11 +1,38 @@
 #!/bin/sh
-cliparams_win="+fonts +window-drag /cert-ignore /kbd:Finnish /sec:nla /bpp:32"
+# Connect to IP address $1 with username $2 and password $3, and consider it a Windows host if $4 is "win[dows]".
+# Or, just use the default values, in which case $1 may be "win[dows]" too.
+
+sound_params=""
+if test "X$(ps -e | grep pulseaudio)" = "X"
+then
+    sound_params=":sys:alsa"
+fi
+
+cliparams_win="+fonts +window-drag /cert-ignore /kbd:Finnish /sec:nla /bpp:32 /sound$sound_params /microphone$sound_params"
 cliparams_other="/bpp:24"
 
-ip=${1:-10.0.1.10}
+default_ip=10.0.1.10
+if test "X$1" = "Xwin" || "X$1" = "Xwindows"
+then
+    os="windows"
+    ip=$default_ip
+else
+    ip=${1:-$default_ip}
+fi
 user=${2:-panther}
 pass=${3:-panther}
-cliparams=$cliparams_other
+
+if test "X$4" = "Xwin" || "X$4" = "Xwindows"
+then
+    os="windows"
+fi
+
+if test "X$os" = "Xwindows"
+then
+    cliparams=$cliparams_win
+else
+    cliparams=$cliparams_other
+fi
 
 panelheight=$(xwininfo -id $(wmctrl -l | grep xfce4-panel | cut -f 1 -d ' ') | grep Height | cut -f 2 -d ":" | tr -d -c [:digit:])
 rootwidth=$(xwininfo -root | grep Width | cut -f 2 -d ":" | tr -d -c [:digit:])
@@ -13,6 +40,7 @@ rootwidth=$(($rootwidth>1920?1920:$rootwidth))
 rootheight=$(xwininfo -root | grep Height | cut -f 2 -d ":" | tr -d -c [:digit:])
 rootheight=$(($rootheight>1200?1200:$rootheight))
 width=$rootwidth
-height=$(expr $rootheight - $panelheight)
+height=$(expr $rootheight - ${panelheight:-0})
 
+set -x
 xfreerdp /v:$ip /size:"$width"x"$height" /network:lan -decorations +clipboard -grab-keyboard /u:$user /p:$pass $cliparams
