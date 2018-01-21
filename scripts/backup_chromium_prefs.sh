1d52d455a7107713963ac9c26bebe10d6ce20518
ohel <ohel@users.noreply.github.com>
2018-01-21 18:15:15 +0200

    More comprehensive backup.

--- scripts/backup_chromium_prefs.sh
+++ scripts/backup_chromium_prefs.sh
@@ -6,22 +6,50 @@ zipname=${1:-config_chromium_default.zip}
 profiledir=${2:-~/.config/chromium/Default}
 cwd=$(pwd)
 
-if ! [ -e $profiledir ];
+if ! [ -e $profiledir ]
 then
     echo "Error: directory $profiledir does not exist."
-    exit
+    echo "Create it? (Press y to create, any other key to exit.)"
+        read -n1 create
+        echo
+        if test "X$create" = "Xy"
+        then
+            mkdir $profiledir
+        else
+            exit
+        fi
+    if ! [ -e $profiledir ]
+    then
+        echo "Unable to create $profiledir."
+        exit
+    fi
 fi
 
 if [ "$#" == 0 ]
 then
     cd $profiledir
-    echo "Zipping Chromium default profile bookmarks, preferences and extensions..."
-    zip -q -r $cwd/$zipname Bookmarks Preferences Extensions
+    echo "Zipping Chromium default profile data."
+    zip -q -r $cwd/$zipname \
+        Extensions \
+        Extension\ State \
+        Extension\ Cookies \
+        Extension\ Rules \
+        Local\ Extension\ Settings \
+        Managed\ Extension\ Settings \
+        Sync\ Extension\ Settings \
+        Local\ Storage\chrome-extension* \
+        databases/chrome-extension* \
+        IndexedDB/chrome-extension* \
+        Login\ Data \
+        Web\ Data \
+        Cookies \
+        Preferences \
+        Bookmarks
     cd $cwd
     echo "Created zip $zipname."
     exit
 fi
 
-echo "Restoring Chromium default profile bookmarks, preferences and extensions from zip..."
+echo "Restoring Chromium default profile data."
 unzip -q "$zipname" -d $profiledir
 echo "Extracted zip $zipname."
