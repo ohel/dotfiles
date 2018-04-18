eae30cee6505f5b268cc63e93433aa6167ae4884
ohel <ohel@users.noreply.github.com>
2018-04-19 02:41:06 +0300

    Fix audio and DVD playback issues.

--- scripts/launchers/media_player.sh
+++ scripts/launchers/media_player.sh
@@ -2,42 +2,40 @@
 # Helper script for launching video files in media player.
 # Must correspond with media player config.
 
-DEFAULTPROFILE="hda"
+DEFAULTPROFILE="general"
 EXE="nice -n -1 /usr/bin/mpv "
 
 if [ "$#" = 0 ]; then
     echo "Usage: media_player.sh [profile shortcut] <filename>"
     echo ""
-    echo "Valid profile shortcuts:  hda  (hda)"
-    echo "                          j    (julia)"
-    echo "                          st   (stereomovie)"
-    echo "                          sr   (surroundmovie)"
-    echo "                          srbm (surroundmovie balanced matrix)"
+    echo "Valid profile shortcuts:"
+    echo "                          general (general)"
+    echo "                          st      (stereomovie)"
+    echo "                          sr      (surroundmovie)"
+    echo "                          srbm    (surroundmoviebalancedmatrix)"
+    echo "                          openal  (openalmovie)"
     echo ""
     echo "If profile shortcut is omitted, $DEFAULTPROFILE profile is assumed."
     exit
 fi
 
-# if no optional arguments are given, just play and exit
 if [ "$#" = 1 ]; then
     $EXE --profile=$DEFAULTPROFILE "$1" &
     exit
 fi
 
-# set profile
 if [ "$1" = "st" ]; then
     profile="stereomovie"
 elif [ "$1" = "sr" ]; then
     profile="surroundmovie"
 elif [ "$1" = "srbm" ]; then
     profile="surroundmoviebalancedmatrix"
-elif [ "$1" = "j" ]; then
-    profile="julia"
-elif [ "$1" = "hda" ]; then
-    profile="hda"
+elif [ "$1" = "openal" ]; then
+    profile="openalmovie"
 else
-    echo "Warning: unknown profile: $1"
+    echo "Using $DEFAULTPROFILE"
     profile=$DEFAULTPROFILE
 fi 
     
-$EXE --profile=$profile "$2" &
+set -x
+$EXE --profile=$profile "${@:2}" &
--- scripts/launchers/media_player_dvd.sh
+++ scripts/launchers/media_player_dvd.sh
@@ -1,45 +1,20 @@
 #!/bin/bash
-# Helper script for launching DVDs (DVD image files) in media player.
-# Must correspond with media player config.
+# Helper script for launching DVDs (or DVD image files).
 # Unfortunately most media players do not support DVD menus anymore.
-
-DEFAULTPROFILE="surroundmovie"
-track=""
-EXE="nice -n -1 /usr/bin/mpv --cache=no"
 
 if [ "$#" = 0 ]; then
     echo "Usage: media_player_dvd.sh [profile shortcut] <filename> [tracknumber]"
-    echo ""
-    echo "Valid profile shortcuts:  st   (stereomovie)"
-    echo "                          sr   (surroundmovie)"
-    echo "                          srbm (surroundmovie balanced matrix)"
-    echo ""
-    echo "If profile shortcut is omitted, $DEFAULTPROFILE profile is assumed."
+    echo "The script calls media_player.sh with DVD parameters as filename."
     exit
 fi
 
-# if no optional arguments are given, just play and exit
+profile="$1"
+device="$2"
+track=$3
+
 if [ "$#" = 1 ]; then
-    $EXE --profile=$DEFAULTPROFILE dvdread:// --dvd-device="$1" &
-    exit
+    profile="sr"
+    device="$1"
 fi
 
-# set profile
-if [ "$1" = "st" ]; then
-    profile="stereomovie"
-elif [ "$1" = "sr" ]; then
-    profile="surroundmovie"
-elif [ "$1" = "srbm" ]; then
-    profile="surroundmoviebalancedmatrix"
-elif [ "$1" = "menu" ]; then
-    profile="stereomovie --input-cursor"
-    track="1"
-else
-    echo "Warning: unknown profile: $1"
-    profile=""
-fi 
-    
-if [ "$#" = 3 ]; then
-    track=$3
-fi
-$EXE ${profile:---profile=$profile} dvdread://$track --dvd-device="$2" &
+~/.scripts/launchers/media_player.sh $profile --cache=no dvdread://$track --dvd-device="$device"
