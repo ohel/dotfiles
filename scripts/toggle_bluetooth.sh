48d130bca71d75ddc0c0d1175efdbcceb184ec0f
ohel <ohel@users.noreply.github.com>
2018-09-04 21:17:36 +0300

    Tweaks.

--- scripts/toggle_bluetooth.sh
+++ scripts/toggle_bluetooth.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-# Toggles bluetooth radio.
+# Toggles bluetooth radio. If $1 == power, toggle power instead of rfkill (block toggle).
+
+if test "X$1" = "Xpower"
+then
+    powermode="true"
+    if test "X$(dbus-send --system --type=method_call --print-reply=literal --dest=org.bluez /org/bluez/hci0 org.freedesktop.DBus.Properties.Get string:org.bluez.Adapter1 string:Powered | grep true)" != "X"
+    then
+        powermode="false"
+    fi
+    dbus-send --system --type=method_call --print-reply=literal --dest=org.bluez /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:org.bluez.Adapter1 string:Powered variant:boolean:$powermode
+    exit
+fi
 
 if [ $(sudo rfkill list bluetooth | grep yes | wc -l) -gt 0 ]
 then
