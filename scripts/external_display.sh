d17c39566739a36b549f0b071bbafb1f78de0aa9
ohel <ohel@users.noreply.github.com>
2018-09-11 15:08:44 +0300

    Font size tweaks.

--- scripts/external_display.sh
+++ scripts/external_display.sh
@@ -13,21 +13,25 @@
 script_mode=${1:-"switch"}
 primary_background=~/.themes/background
 secondary_background=~/.themes/background2
-
-primary_display=${2:-$(xrandr | grep " connected" | cut -f 1 -d ' ' | head -n 1)}
-primary_mode=$(xrandr | grep -A 1 $primary_display | tail -n 1 | tr -s ' ' | cut -f 2 -d ' ')
-echo "Primary display: $primary_display with mode $primary_mode."
-
 testfile=/tmp/external_display
 
+primary_display=${2:-$(xrandr | grep " connected" | cut -f 1 -d ' ' | head -n 1)}
+primary_mode=$(xrandr | grep -A 1 $primary_display | grep -o "[0-9]\{3,4\}x[0-9]\{3,4\}" | tail -n 1)
+echo "Primary display: $primary_display with mode $primary_mode."
+p_width=$(echo $primary_mode | cut -f 1 -d 'x')
+
 secondary_display=$(xrandr | grep " connected" | cut -f 1 -d ' ' | grep -v $primary_display | head -n 1)
-[ "$secondary_display" != "" ] && secondary_mode=$(xrandr | grep -A 1 $secondary_display | tail -n 1 | tr -s ' ' | cut -f 2 -d ' ')
+if [ "$secondary_display" != "" ]
+then
+    secondary_mode=$(xrandr | grep -A 1 $secondary_display | grep -o "[0-9]\{3,4\}x[0-9]\{3,4\}" | tail -n 1)
+    s_width=$(echo $secondary_mode | cut -f 1 -d 'x')
+fi
 
 if [ -f $testfile ]
 then
     echo "Switching off external display $secondary_display."
-    xrandr --output $primary_display --mode $primary_mode --primary
-    xrandr --output $secondary_display --off
+    xrandr --output $primary_display --mode $primary_mode --primary \
+        --output $secondary_display --off
     secondary_background=""
     rm $testfile
 elif [ "$secondary_mode" != "" ]
@@ -35,15 +39,15 @@ then
     if [ "$script_mode" == "switch" ]
     then
         echo "Switching to external display $secondary_display."
-        xrandr --output $secondary_display --mode $secondary_mode --primary
-        xrandr --output $primary_display --off
+        xrandr --output $secondary_display --mode $secondary_mode --primary \
+            --output $primary_display --off
         secondary_background=""
     elif [ "$script_mode" == "extend" ]
     then
         echo "Extending to external display $secondary_display."
-        xrandr --output $primary_display --mode $primary_mode --primary
         position=${3:-"right-of"}
-        xrandr --output $secondary_display --mode $secondary_mode --$position $primary_display
+        xrandr --output $primary_display --mode $primary_mode --primary \
+            --output $secondary_display --mode $secondary_mode --$position $primary_display
         panelwin="xfce4-panel"
         panelheight=$(xwininfo -id $(wmctrl -l | grep $panelwin | cut -f 1 -d ' ') | grep Height | cut -f 2 -d ":" | tr -d -c [:digit:])
         panel_y=$(echo $(echo $primary_mode | cut -f 2 -d 'x') - $panelheight | bc)
@@ -51,21 +55,43 @@ then
     elif [ "$script_mode" == "mirror" ]
     then
         echo "Mirroring to external display $secondary_display."
-        p_width=$(echo $primary_mode | cut -f 1 -d 'x')
         p_height=$(echo $primary_mode | cut -f 2 -d 'x')
-        s_width=$(echo $secondary_mode | cut -f 1 -d 'x')
         s_height=$(echo $secondary_mode | cut -f 2 -d 'x')
         mirror_width=$p_width
         mirror_height=$p_height
         [ $s_width -lt $p_width ] && mirror_width=$s_width
         [ $s_height -lt $p_height ] && mirror_height=$s_height
         mirror_mode=$mirror_width"x"$mirror_height
-        xrandr --output $primary_display --mode $mirror_mode --noprimary
-        xrandr --output $secondary_display --mode $mirror_mode
+        xrandr --output $primary_display --mode $mirror_mode --noprimary \
+            --output $secondary_display --mode $mirror_mode
         secondary_background=""
         echo "Set mode $mirror_mode."
     fi
     touch $testfile
 fi
 
+# Xrandr only shows the physical size if the display is connected, therefore we need to call xrandr again.
+# The DPI values are arbitrarily chosen and based on personal preference and commonly used hardware.
+p_phys_width=$(xrandr | grep -A 1 $primary_display | grep -o [0-9]*mm | head -n 1 | tr -d [:alpha:])
+if [ "$p_phys_width" != "" ]
+then
+    p_dpi_calc=$(echo "scale=2; $p_width / $p_phys_width * 25.4" | bc | cut -f 1 -d '.')
+    p_dpi_set=96
+    [ $p_dpi_calc -gt 100 ] && p_dpi_set=112
+    [ $p_dpi_calc -gt 140 ] && p_dpi_set=144
+    common_dpi=$p_dpi_set
+fi
+
+[ "$secondary_display" != "" ] && s_phys_width=$(xrandr | grep -A 1 $secondary_display | grep -o [0-9]*mm | head -n 1 | tr -d [:alpha:])
+if [ "$s_phys_width" != "" ]
+then
+    s_phys_width=$(xrandr | grep -A 1 $secondary_display | grep -o [0-9]*mm | head -n 1 | tr -d [:alpha:])
+    s_dpi_calc=$(echo "scale=2; $s_width / $s_phys_width * 25.4" | bc | cut -f 1 -d '.')
+    s_dpi_set=96
+    [ $s_dpi_calc -gt 100 ] && s_dpi_set=112
+    [ $s_dpi_calc -gt 140 ] && s_dpi_set=144
+    ([ "$common_dpi" == "" ] || [ $s_dpi_set -lt $p_dpi_set ]) && common_dpi=$s_dpi_set
+fi
+
+which xfconf-query &>/dev/null && xfconf-query -c xsettings -p /Xft/DPI -s $common_dpi
 which feh &>/dev/null && feh --bg-fill --no-fehbg $primary_background $secondary_background 2>/dev/null
