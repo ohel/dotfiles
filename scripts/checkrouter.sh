5573de5e85d1c51218a98d940e7b1c2c3b134b29
ohel <ohel@users.noreply.github.com>
2018-09-13 01:53:00 +0300

    Add router reboot scripts.

---
+++ scripts/checkrouter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# When using an unstable consumer class router that likes to break down
+# every now and then, detect when it's behaving badly and reboot it
+# by calling a script with router model parameter.
+
+routermodel=${1:-fast}
+
+routerip=$(ip route | grep default | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}")
+
+echo "Pinging router to check if reboot required..."
+[ "$(ping -c 10 $routerip | grep ' 0% packet loss')" != "" ] && echo "Router seems OK." && exit
+
+echo "Packet loss detected. Going to reboot router."
+
+scriptsdir=$(dirname "$(readlink -f "$0")")
+setsid xfce4-terminal -e "bash -c '$scriptsdir/rebootrouter.sh $routermodel'"
--- scripts/rebootrouter.sh
+++ scripts/rebootrouter.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-# When using a router with dynamic WAN IPv4 address, access router status page and get the address.
+# Reboot an unstable consumer class router.
 # Supports the following router models (pass as parameter):
-#    tw: Telewell TW-LTE/4G/3G router, WiFI AC
 #    fast: Sagemcom FAST3686 (DNA Valokuitu Plus)
 
 routermodel=${1:-fast}
 
 routerip=$(ip route | grep default | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}")
-username=$(whoami)
 
 readpw() {
     if [ -e ~/.config/router_password ]
@@ -22,13 +20,10 @@ readpw() {
     stty $stty_orig
 }
 
-if [ "$routermodel" == "tw" ]
-then
-    readpw
-    wanipv4=$(wget http://$routerip/adm/status.asp --user=$username --password=$pw -q -O - \
-        | grep 'id="idv4wanip"' \
-        | grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}")
-elif [ "$routermodel" == "fast" ]
+echo "Preparing to reboot router at $routerip. Press return to continue."
+read
+
+if [ "$routermodel" == "fast" ]
 then
     # Check if login is needed. The router remembers logged in computers with some kind of logic.
     responsepage=$(wget http://$routerip/RgSetup.asp -q -O -)
@@ -40,28 +35,22 @@ then
         skey=$(echo $responsepage | grep -o "SessionKey = [0-9]*;" | tr -dc "[:digit:]")
         wget -q -O /dev/null "http://$routerip/goform/login?sessionKey=$skey" \
             --post-data="loginUsername=$username&loginPassword=$pw"
-        responsepage=$(wget http://$routerip/RgSetup.asp -q -O -)
     fi
-    wanipv4=$(echo $responsepage | grep -o "id=\"wanipaddr[^<]*" | cut -f 2 -d '>')
+    responsepage=$(wget http://$routerip/RgSetup.asp -q -O -)
+    skey=$(echo $responsepage | grep -o "SessionKey = [0-9]*;" | tr -dc "[:digit:]")
+    echo Sending reboot request...
+    # The WanConnectionType is needed in data, otherwise the request will hang.
+    wget -q -O /dev/null "http://$routerip/goform/RgSetup?sessionKey=$skey" \
+        --header "Referer: http://$routerip/RgSetup.asp" \
+        --post-data="WanConnectionType=0&RebootAction=1"
 fi
 
-echo
-echo WAN IPv4: $wanipv4
-echo
-
-if [ "$wanipv4" == "" ]
-then
-    echo "Error retrieving IPv4."
-    exit 1
-fi
-
-# Use a secret script to upload contents of $ip variable somewhere publicly available.
-if [ -e ~/.scripts_extra/publish_ip.sh ]
-then
-    echo -n "Press return to publish WAN IPv4."
-    read
-
-    sh ~/.scripts_extra/publish_ip.sh $wanipv4 2>/dev/null
-
-    echo "Done."
-fi
+echo Rebooting...
+sleep 30
+echo Pinging router...
+while [ "$pingresponse" != "" ]
+do
+    pingresponse=$(ping -c 1 $routerip 2>/dev/null | grep " 0% packet loss")
+done
+echo Got ping response, router is up. Press return to exit.
+read
