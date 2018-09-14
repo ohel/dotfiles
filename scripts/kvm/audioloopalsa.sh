e11a48e140aeed942e09ac94825a075a51cdf169
ohel <ohel@users.noreply.github.com>
2018-09-15 02:57:30 +0300

    Add buffer size for less lag.

--- scripts/kvm/audioloopalsa.sh
+++ scripts/kvm/audioloopalsa.sh
@@ -1,3 +1,3 @@
-arecord -r 44100 -c 2 -f S16_LE -D loop_vm_dac_out | aplay &
-arecord -r 44100 -c 2 -f S16_LE -D julia_ain_mix_44100 | aplay -D loop_vm_adc_in
-
+buffer_size=512
+arecord -r 44100 -c 2 -f S16_LE -D loop_vm_dac_out --buffer-size=$buffer_size | aplay --buffer-size=$buffer_size &
+arecord -r 44100 -c 2 -f S16_LE -D julia_ain_mix_44100 --buffer-size=$buffer_size | aplay -D loop_vm_adc_in --buffer-size=$buffer_size
