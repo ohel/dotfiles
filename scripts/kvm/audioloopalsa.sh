c4a3bddd912a158b2329aa24f9f4ccfe9160d2bb
ohel <ohel@users.noreply.github.com>
2018-09-15 03:08:19 +0300

    Tweaks.

--- scripts/kvm/audioloopalsa.sh
+++ scripts/kvm/audioloopalsa.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+# Connect ALSA loopback devices to default audio output and input devices.
 buffer_size=512
 arecord -r 44100 -c 2 -f S16_LE -D loop_vm_dac_out --buffer-size=$buffer_size | aplay --buffer-size=$buffer_size &
-arecord -r 44100 -c 2 -f S16_LE -D julia_ain_mix_44100 --buffer-size=$buffer_size | aplay -D loop_vm_adc_in --buffer-size=$buffer_size
+arecord -r 44100 -c 2 -f S16_LE --buffer-size=$buffer_size | aplay -D loop_vm_adc_in --buffer-size=$buffer_size
--- scripts/kvm/audioloopjack.sh
+++ scripts/kvm/audioloopjack.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+# Connect ALSA loopback devices to JACK.
 alsa_in -j "VM output" -dloop_vm_dac_out &
 alsa_out -j "VM input" -dloop_vm_adc_in
