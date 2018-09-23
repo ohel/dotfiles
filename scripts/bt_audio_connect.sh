872cc674199cafa5d41f01413785c9cb833d7184
ohel <ohel@users.noreply.github.com>
2018-09-24 00:19:11 +0300

    Add .asoundrc support.

--- scripts/bt_audio_connect.sh
+++ scripts/bt_audio_connect.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# Connect to device with MAC address ALSA_BLUETOOTH_MAC as set in the environment.
+# Connect to device with MAC address ALSA_BLUETOOTH_MAC (environment variable).
+# Prefer pcm.bluetooth device MAC if defined in ~/.asoundrc or /etc/asound.conf.
 # Uses a helper script to connect.
 # Assumes BlueALSA is running.
 
-if [ ! "$ALSA_BLUETOOTH_MAC" ]
+mac=$(cat .asoundrc 2>/dev/null | grep -A 20 "pcm\.bluetooth" | grep -o "\(..:\)\{5\}.." | grep -v "[0:]\{17\}")
+[ $mac ] || mac=$(cat /etc/asound.conf 2>/dev/null | grep -A 20 "pcm\.bluetooth" | grep -o "\(..:\)\{5\}.." | grep -v "[0:]\{17\}")
+[ $mac ] || mac=$ALSA_BLUETOOTH_MAC
+
+if [ ! "$mac" ]
 then
     echo "Define the environment variable ALSA_BLUETOOTH_MAC first."
     sleep 1
@@ -30,12 +35,12 @@ scriptsdir=$(dirname "$(readlink -f "$0")")
 retries=3
 while [ $retries -gt 0 ]
 do
-    if $scriptsdir/bt_dev_connect.sh $ALSA_BLUETOOTH_MAC
+    if $scriptsdir/bt_dev_connect.sh $mac
     then
         seconds=10
         while [ $seconds -gt 0 ]
         do
-            ! checkconnection $ALSA_BLUETOOTH_MAC && echo "Connected to $ALSA_BLUETOOTH_MAC" && exit 0
+            ! checkconnection $mac && echo "Connected to $mac " && exit 0
             sleep 1
             seconds=$(expr $seconds - 1)
         done
