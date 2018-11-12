8acefd6208cbcf8e55513942bbab10fc03d941ad
ohel <ohel@users.noreply.github.com>
2018-11-12 02:40:03 +0200

    Tweaks.

--- scripts/matrix_orbital/lcd_glow.sh
+++ scripts/matrix_orbital/lcd_glow.sh
@@ -2,34 +2,28 @@
 # Make an LCD device backlight glow.
 
 dev=/dev/serial/matrix_orbital
-int=0.08
+int=0.1
 
 while [ 1 ]
 do
     echo -en "\xFE\x99\x00" > $dev; sleep $int
+    echo -en "\xFE\x99\x01" > $dev; sleep $int
+    echo -en "\xFE\x99\x03" > $dev; sleep $int
+    echo -en "\xFE\x99\x07" > $dev; sleep $int
     echo -en "\xFE\x99\x0F" > $dev; sleep $int
-    echo -en "\xFE\x99\x23" > $dev; sleep $int
-    echo -en "\xFE\x99\x37" > $dev; sleep $int
-    echo -en "\xFE\x99\x4B" > $dev; sleep $int
+    echo -en "\xFE\x99\x1F" > $dev; sleep $int
+    echo -en "\xFE\x99\x3F" > $dev; sleep $int
     echo -en "\xFE\x99\x5F" > $dev; sleep $int
-    echo -en "\xFE\x99\x73" > $dev; sleep $int
-    echo -en "\xFE\x99\x87" > $dev; sleep $int
-    echo -en "\xFE\x99\x9B" > $dev; sleep $int
-    echo -en "\xFE\x99\xAF" > $dev; sleep $int
-    echo -en "\xFE\x99\xC3" > $dev; sleep $int
-    echo -en "\xFE\x99\xD7" > $dev; sleep $int
-    echo -en "\xFE\x99\xEB" > $dev; sleep $int
+    echo -en "\xFE\x99\x7F" > $dev; sleep $int
+    echo -en "\xFE\x99\xBF" > $dev; sleep $int
     echo -en "\xFE\x99\xFF" > $dev; sleep $int
-    echo -en "\xFE\x99\xEB" > $dev; sleep $int
-    echo -en "\xFE\x99\xD7" > $dev; sleep $int
-    echo -en "\xFE\x99\xC3" > $dev; sleep $int
-    echo -en "\xFE\x99\xAF" > $dev; sleep $int
-    echo -en "\xFE\x99\x9B" > $dev; sleep $int
-    echo -en "\xFE\x99\x87" > $dev; sleep $int
-    echo -en "\xFE\x99\x73" > $dev; sleep $int
+    echo -en "\xFE\x99\xBF" > $dev; sleep $int
+    echo -en "\xFE\x99\x7F" > $dev; sleep $int
     echo -en "\xFE\x99\x5F" > $dev; sleep $int
-    echo -en "\xFE\x99\x4B" > $dev; sleep $int
-    echo -en "\xFE\x99\x37" > $dev; sleep $int
-    echo -en "\xFE\x99\x23" > $dev; sleep $int
+    echo -en "\xFE\x99\x3F" > $dev; sleep $int
+    echo -en "\xFE\x99\x1F" > $dev; sleep $int
     echo -en "\xFE\x99\x0F" > $dev; sleep $int
+    echo -en "\xFE\x99\x07" > $dev; sleep $int
+    echo -en "\xFE\x99\x03" > $dev; sleep $int
+    echo -en "\xFE\x99\x01" > $dev; sleep $int
 done
