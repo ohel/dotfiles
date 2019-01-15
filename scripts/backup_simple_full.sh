3e5d4fa105e2d8a6052e7cc21d085e9c02134b3e
ohel <ohel@users.noreply.github.com>
2019-01-15 18:54:13 +0200

    Add new backup script.

---
+++ scripts/backup_simple_full.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Simple full system backup into a mountpoint $1 using rsync.
+# Optionally backup is done using prefix $2 (useful for offline system backup).
+
+backupmount=${1:-"/mnt/usb-backup"}
+prefix="$2"
+
+[ ! "$(mount | grep $backupmount)" ] && echo "$backupmount not mounted" && exit 1
+[ "$(whoami)" != "root" ] && echo "$(whoami) != root" && exit 1
+
+rsync -avu --delete \
+  --exclude $prefix/dev/shm/\* \
+  --exclude $prefix/media/\* \
+  --exclude $prefix/mnt/\* \
+  --exclude $prefix/proc/\* \
+  --exclude $prefix/run/\* \
+  --exclude $prefix/sys/\* \
+  --exclude $prefix/tmp/\* \
+  $prefix/ $backupmount/
