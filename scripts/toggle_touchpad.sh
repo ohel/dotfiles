1adc44b353750c9d0b937199512b72efd4e22de8
ohel <ohel@users.noreply.github.com>
2019-03-13 17:57:23 +0200

    Support Synaptics touchpad.

--- scripts/toggle_touchpad.sh
+++ scripts/toggle_touchpad.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-# Two ways to enable or disable an Elan touchpad.
-
-#id=$(xinput | grep Elan | cut -f 2 -d '=' | cut -f 1)
-#
-#status=$(xinput list-props "$id" | grep "Device Enabled" | grep -Eo '.$')
-#if [ $status = 0 ]
-#then
-#    xinput set-prop "$id" "Device Enabled" 1
-#else 
-#    xinput set-prop "$id" "Device Enabled" 0
-#fi
+# Enable or disable a touchpad. Do this via kernel module if Elan touchpad or via xinput if Synaptics.
 
 if [ "$(lsmod | grep elan_i2c)" ]
 then
     sudo modprobe -r elan_i2c
 else
-    sudo modprobe elan_i2c
+    synaptics_id=$(xinput | grep Synaptics | cut -f 2 -d '=' | cut -f 1)
+    [ "$synaptics_id" ] && status=$(xinput list-props "$synaptics_id" | grep "Device Enabled" | grep -o ".$")
+
+    if [ ! "$status" ]
+    then
+        sudo modprobe elan_i2c
+    else
+        [ $status = 0 ] && xinput set-prop "$synaptics_id" "Device Enabled" 1
+        [ $status = 1 ] && xinput set-prop "$synaptics_id" "Device Enabled" 0
+    fi
 fi
