eb6d5c63742051b22890e18d976537ce255c2caa
ohel <ohel@users.noreply.github.com>
2019-10-20 17:59:51 +0300

    Support feh.

--- scripts/switch_background.sh
+++ scripts/switch_background.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-# Toggle alternative background using xfdesktop.
+# Toggle alternative background using feh or reloading xfdesktop.
 # The alternative background kills conky if in use.
 
 if [ "$(basename $(readlink -f ~/.themes/background))" != "background.png" ]
 then
-    conkylauncher=~/.scripts/launchers/conky.sh
     ln -sf ~/.themes/background.png ~/.themes/background
+    conkylauncher=~/.scripts/launchers/conky.sh
     [ -e $conkylauncher ] && setsid $conkylauncher
 else
     ln -sf ~/.themes/background_alt.png ~/.themes/background
     killall conky
 fi
 
-which xfdesktop 2>/dev/null && setsid xfdesktop --reload
+which feh 2>/dev/null && feh --no-fehbg --bg-fill ~/.themes/background
+which xfdesktop 2>/dev/null && setsid xfdesktop --reload && exit 0
