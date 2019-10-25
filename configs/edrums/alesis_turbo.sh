5809f9b1ffcd40f1b12604fbe25e7881d5bda9b4
ohel <ohel@users.noreply.github.com>
2019-10-25 22:05:50 +0300

    Tweaks to edrum configs.

--- configs/edrums/alesis_turbo.sh
+++ configs/edrums/alesis_turbo.sh
@@ -9,6 +9,18 @@ sleep 1
 # mididings_script is originally named mididings, but there's a directory by the same name for Python code
 /opt/programs/mididings/mididings_script -f $scriptdir/alesis_turbo.py &
 sleep 1
+echo "Connecting MIDI"
 aconnect "Alesis Turbo" mididings
 aconnect mididings:1 Hydrogen
-wait
+echo "Connecting ALSA/JACK bridge"
+alsa_in -j "ALSA output" -d loop_playback_out &
+alsa_out -j "ALSA input" -d loop_record_in &
+sleep 1
+jack_connect "ALSA output:capture_1" system:playback_1
+jack_connect "ALSA output:capture_2" system:playback_2
+echo
+echo Press return twice to quit.
+read temp
+echo Press return once to quit.
+read temp
+killall jackd
