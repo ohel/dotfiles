8bddabd62b7fcb4661f5ba3914fd575769cf33fd
ohel <ohel@users.noreply.github.com>
2020-01-06 14:06:35 +0200

    Python version bump.

--- scripts/launchers/ccsm.sh
+++ scripts/launchers/ccsm.sh
@@ -2,6 +2,7 @@
 # CompizConfig Settings Manager launcher.
 # Compiz 0.9 series with prefix /opt/programs/compiz.
 
+pv=$(python -V | grep -o "3\.[0-9]")
 env LD_LIBRARY_PATH=/opt/programs/compiz/lib64 \
-PYTHONPATH=/opt/programs/compiz/lib/python2.7/site-packages:/opt/programs/compiz/lib64/python2.7/site-packages \
+PYTHONPATH=/opt/programs/compiz/lib/python$pv/site-packages:/opt/programs/compiz/lib64/python$pv/site-packages \
 /opt/programs/compiz/bin/ccsm &
