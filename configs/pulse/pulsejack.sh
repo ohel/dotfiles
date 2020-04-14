1f2fe94db8f0b980985903e8ade51513f6bf3ff3
ohel <ohel@users.noreply.github.com>
2020-04-14 03:13:08 +0300

    Tweaks.

--- configs/pulse/pulsejack.sh
+++ configs/pulse/pulsejack.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # Run JACK daemon if not running and start PulseAudio as a JACK pipe.
 
+device="hw:M4"
+
 if [ ! "$(ps -e | grep jackd)" ]
 then
-    jackd -T -dalsa -r44100 -p256 -P hw:M4 -C hw:M4 &
+    jackd -T -dalsa -r44100 -p256 -P $device -C $device &
     sleep 1
     echo
 fi
+
 echo "Press Ctrl-C to stop PulseAudio."
 pulseaudio -n -F /etc/pulse/jackpipe.pa
