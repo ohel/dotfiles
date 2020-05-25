b503dc814da4bd95c07331fe116bcab7190445db
ohel <ohel@users.noreply.github.com>
2020-05-25 04:37:06 +0300

    Support different executable names.

--- scripts/launchers/chromium.sh
+++ scripts/launchers/chromium.sh
@@ -5,6 +5,11 @@
 # - open a speed dial page to new tab if no URL given
 # - open the given URL or a new Chromium window with newwindow parameter
 
+# Chromium executable name varies between systems.
+[ "$(which chromium-browser 2>/dev/null)" ] && executable="chromium-browser"
+[ ! "$executable" ] && [ "$(which chromium 2>/dev/null)" ] && executable="chromium"
+[ ! "$executable" ] && echo "Chromium not found." && exit 1
+
 focuswin=$(wmctrl -l | grep -e "Chromium" | tail -n 1 | cut -f 1 -d ' ')
 if [ "$focuswin" ]
 then
@@ -13,16 +18,16 @@ fi
 
 if [ "$1" = "newwindow" ]
 then
-    chromium-browser
+    $executable
     exit
 elif [ "$#" -eq 1 ]
 then
-    chromium-browser "$1"
+    $executable "$1"
 elif [ ! "$(ps -ef | grep chromium-browser | grep -v grep)" ]
 then
-    chromium-browser
+    $executable
 else
-    # To open a blank page: chromium-browser about:blank
+    # To open a blank page: $executable about:blank
     # Instead of opening a blank page, use Ctrl+t new tab shortcut to open speed dial page.
     # Assuming Win+w is the shortcut for this script, keyup w first.
     # We first have to sleep for a tiny bit for the window to get focus.
