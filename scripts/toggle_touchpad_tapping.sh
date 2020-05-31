7eeb7655b20d6b6caca915e4d08ece96579141be
ohel <ohel@users.noreply.github.com>
2020-05-31 21:52:45 +0300

    Support Synaptics in touchpad name.

--- scripts/toggle_touchpad_tapping.sh
+++ scripts/toggle_touchpad_tapping.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Toggles an Elan, Alps etc. touchpad tap-to-click.
 
-id=$(xinput | grep "\(Elan\)\|\(TouchPad\)" | cut -f 2 -d '=' | cut -f 1)
+id=$(xinput | grep "\(Elan\)\|\(TouchPad\)\|\(Synaptics\)" | cut -f 2 -d '=' | cut -f 1)
 
 status=$(xinput list-props "$id" | grep "Synaptics Tap Action" | grep -Eo '.$')
 if [ "$status" = "0" ]
 then
     xinput set-prop "$id" "Synaptics Tap Action" 2 3 0 0 1 3 2
-else 
+else
     xinput set-prop "$id" "Synaptics Tap Action" 2 3 0 0 0 0 0
 fi
