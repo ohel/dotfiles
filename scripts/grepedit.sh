63d82df0c0d93ce0306a0dbee640a4f90b9afec1
ohel <ohel@users.noreply.github.com>
2020-06-12 01:04:31 +0300

    Fix ripgrep params.

--- scripts/grepedit.sh
+++ scripts/grepedit.sh
@@ -10,7 +10,7 @@ ag_exe=$(which ag 2>/dev/null)
 
 [ "$#" -eq 0 ] && exit 1
 
-rg $1 2>/dev/null || ag $1 2>/dev/null || grep -n -R -i --color=auto $1 2>/dev/null
+rg -S -uu $1 2>/dev/null || ag $1 2>/dev/null || grep -n -R -i --color=auto $1 2>/dev/null
 
 echo Enter search terms for filenames. All terms must match the name to edit a file.
 echo -n "Search for: "
@@ -23,6 +23,6 @@ done
 
 cmd="grep -l -R $1 . $greps | xargs $editor"
 [ "$ag_exe" ] && cmd="ag -l --nogroup --nocolor --column $1 . $greps | xargs $editor"
-[ "$rg_exe" ] && cmd="rg -l --no-heading --with-filename --color never --column $1 . $greps | xargs $editor"
+[ "$rg_exe" ] && cmd="rg -S -uu -l --no-heading --with-filename --color never --column $1 . $greps | xargs $editor"
 
 eval $(echo $cmd)
