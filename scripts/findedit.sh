3b3d3a9d98cb63e891d21f0f65a188197db09db0
ohel <ohel@users.noreply.github.com>
2020-10-26 12:33:17 +0200

    Support spaces.

--- scripts/findedit.sh
+++ scripts/findedit.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
-# List files using find grepping $1 from their names, then select a search result to edit.
+# List files using find, grepping "$1" from their names, then select a search result to edit.
 # If just one result is found, it is opened automatically.
 
+editor=$EDITOR
 [ "$(which gedit 2>/dev/null)" ] && editor=gedit
 [ "$(which vim 2>/dev/null)" ] && editor=vim
 [ "$(which gvim 2>/dev/null)" ] && editor=gvim
 
 [ "$#" -eq 0 ] && echo "Give the search term as a parameter." && exit 1
 
-results=($(find ./ | grep -i $1))
+results=$(find ./ | grep -i "$1")
 
-if [ ${#results[@]} -eq 0 ]
+if [ ! "$results" ]
 then
     echo "No matches."
     exit 0
-elif [ ${#results[@]} -eq 1 ]
+elif [ $(echo "$results" | wc -l) -eq 1 ]
 then
-    $editor ${results[0]}
+    $editor "$results"
     exit 0
 fi
 
-order=1
-for result in ${results[@]}
+count=$(echo "$results" | wc -l)
+
+index=1
+while [ $index -le $count ]
 do
-    echo $order: $result :$order
-    order=$(expr $order + 1)
+    echo $index: $(echo "$results" | head -n $index | tail -n 1) :$index
+    index=$(expr $index + 1)
 done
 
 echo -n "Edit the one with order number: "
@@ -32,6 +35,5 @@ read -a index
 
 index=$(echo $index | tr -c -d [:digit:])
 ! [ "$index" ] && exit 1
-index=$(expr $index - 1)
 
-$editor ${results[$index]}
+$editor "$(echo "$results" | head -n $index | tail -n 1)"
--- scripts/grepedit.sh
+++ scripts/grepedit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Search stuff using rg/ag/grep, then from the search results open filenames matching search terms.
+# Search stuff using rg/ag/grep "$1", then from the search results open filenames matching search terms.
 
 [ "$(which gedit 2>/dev/null)" ] && editor="gedit"
 [ "$(which vim 2>/dev/null)" ] && editor="vim"
@@ -10,7 +10,7 @@ ag_exe=$(which ag 2>/dev/null)
 
 [ "$#" -eq 0 ] && exit 1
 
-rg -S -uu $1 2>/dev/null || ag $1 2>/dev/null || grep -n -R -i --color=auto $1 2>/dev/null
+rg -S -uu "$1" 2>/dev/null || ag "$1" 2>/dev/null || grep -n -R -i --color=auto "$1" 2>/dev/null
 
 echo Enter search terms for filenames. All terms must match the name to edit a file.
 echo -n "Search for: "
@@ -21,8 +21,8 @@ do
     greps="$greps | grep -i $word"
 done
 
-cmd="grep -l -R $1 . $greps | xargs $editor"
-[ "$ag_exe" ] && cmd="ag -l --nogroup --nocolor --column $1 . $greps | xargs $editor"
-[ "$rg_exe" ] && cmd="rg -S -uu -l --no-heading --with-filename --color never --column $1 . $greps | xargs $editor"
+cmd="grep -l -R \"$1\" . $greps | xargs $editor"
+[ "$ag_exe" ] && cmd="ag -l --nogroup --nocolor --column \"$1\" . $greps | xargs $editor"
+[ "$rg_exe" ] && cmd="rg -S -uu -l --no-heading --with-filename --color never --column \"$1\" . $greps | xargs $editor"
 
 eval $(echo $cmd)
