0d45557b4fa7536e7ba7bab5de3adf9dccf3ee69
ohel <ohel@users.noreply.github.com>
2021-05-02 01:22:13 +0300

    Enhancements.

--- scripts/extract_archive.sh
+++ scripts/extract_archive.sh
@@ -5,10 +5,12 @@ filename="$1"
 type_zip=$(file "$filename" | grep Zip)
 type_rar=$(file "$filename" | grep RAR)
 type_gzip=$(file "$filename" | grep gzip)
+type_bzip=$(file "$filename" | grep gzip)
 type_tar=$(file "$filename" | grep tar)
 type_7z=$(file "$filename" | grep 7-zip)
-[ "$type_zip" ] && unzip -d "$(echo "$filename" | sed "s/.zip//")" -x "$filename"
+[ "$type_zip" ] && unzip -d "$(echo "$filename" | sed "s/\.zip$//")" -x "$filename"
 [ "$type_rar" ] && rar x "$filename"
 [ "$type_gzip" ] && tar xzf "$filename"
+[ "$type_bzip" ] && tar xjf "$filename"
 [ "$type_tar" ] && tar xf "$filename"
 [ "$type_7z" ] && 7z x "$filename"
--- scripts/view_archive.sh
+++ scripts/view_archive.sh
@@ -5,10 +5,14 @@ filename="$1"
 type_zip=$(file "$filename" | grep Zip)
 type_rar=$(file "$filename" | grep RAR)
 type_gzip=$(file "$filename" | grep gzip)
+type_bzip=$(file "$filename" | grep bzip2)
 type_tar=$(file "$filename" | grep tar)
+type_7z=$(file "$filename" | grep 7-zip)
 done=""
 
 [ ! "$done" ] && [ "$type_zip" ] && done=1 && unzip -l "$filename" | gvim -R -
 [ ! "$done" ] && [ "$type_rar" ] && done=1 && unrar vp "$filename" | gvim -R -
 [ ! "$done" ] && [ "$type_gzip" ] && done=1 && tar ztvf "$filename" | gvim -R -
+[ ! "$done" ] && [ "$type_bzip" ] && done=1 && tar jtvf "$filename" | gvim -R -
 [ ! "$done" ] && [ "$type_tar" ] && done=1 && tar tvf "$filename" | gvim -R -
+[ ! "$done" ] && [ "$type_7z" ] && done=1 && 7z l "$filename" | gvim -R -
