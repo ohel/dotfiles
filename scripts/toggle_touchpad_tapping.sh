aed5722ccccc45b63d76d4853d79aa616c6b0275
ohel <ohel@users.noreply.github.com>
2021-09-16 00:29:12 +0300

    Some fixes/improvements, some cleanup.

--- scripts/toggle_touchpad_tapping.sh
+++ scripts/toggle_touchpad_tapping.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# Toggles an Elan, Alps etc. touchpad tap-to-click.
+# Toggles an Elan, Alps etc. touchpad tap-to-click. Disable if $1 = disable.
 
-id=$(xinput | grep "\(Elan\)\|\(TouchPad\)\|\(Synaptics\)" | cut -f 2 -d '=' | cut -f 1)
+id=$(xinput | grep -i "\(touchpad\)\|\(synaptics\)" | cut -f 2 -d '=' | cut -f 1)
 
 status=$(xinput list-props "$id" | grep "Synaptics Tap Action" | grep -Eo '.$')
-if [ "$status" = "0" ]
+if [ "$status" = "0" ] && [ "$1" != "disable" ]
 then
     xinput set-prop "$id" "Synaptics Tap Action" 2 3 0 0 1 3 2
 else
