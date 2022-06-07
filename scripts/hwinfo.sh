33e3936043930296248448f67779532cf877d226
ohel <ohel@users.noreply.github.com>
2022-06-07 16:49:46 +0300

    Add some plugins.

--- scripts/hwinfo.sh
+++ scripts/hwinfo.sh
@@ -5,8 +5,8 @@ num_cores=$(grep "cpu cores" /proc/cpuinfo | head -n 1 | cut -f 2 -d ':' | tr -d
 echo "CPU core frequencies (showing $num_cores real cores):"
 grep MHz /proc/cpuinfo | head -n $num_cores
 echo
-echo CPU core temperatures:
-sensors 2>/dev/null | grep "\(Core [0-9]\)\|\(Tdie\)"
+echo "CPU core & Core Complex Die temperatures:"
+sensors 2>/dev/null | grep "\(Core [0-9]\)\|\(Tdie\)\|\(Tccd\)"
 echo
 echo Graphics card:
 sensors 2>/dev/null | grep -A 6 amdgpu | grep "\(temp\)\|\(edge\)" | cut -f 2 -d ':'
