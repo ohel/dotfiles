b19cde6c7b21ee6371179bac762101dd32fac616
ohel <ohel@users.noreply.github.com>
2022-10-25 12:54:54 +0300

    Minor tweaks/checks/fixes.

--- scripts/launchers/ccsm.sh
+++ scripts/launchers/ccsm.sh
@@ -2,7 +2,7 @@
 # CompizConfig Settings Manager launcher.
 # Compiz 0.9 series with prefix /opt/programs/compiz.
 
-pv=$(python -V | grep -o "3\.[0-9]")
+pv=$(python -V | grep -o "3\.[0-9]\{1,\}")
 env LD_LIBRARY_PATH=/opt/programs/compiz/lib64 \
 PYTHONPATH=/opt/programs/compiz/lib/python$pv/site-packages:/opt/programs/compiz/lib64/python$pv/site-packages \
 /opt/programs/compiz/bin/ccsm &
