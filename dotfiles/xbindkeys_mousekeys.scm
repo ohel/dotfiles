5c37ddf729a0155e2ebac5154684a1cbcb59d019
ohel <ohel@users.noreply.github.com>
2023-03-09 15:17:14 +0200

    Mouse config tweaks.

--- dotfiles/xbindkeys_mousekeys.scm
+++ dotfiles/xbindkeys_mousekeys.scm
@@ -3,6 +3,7 @@
 ; Mouse events are not registered if using a simple control key mapping (xdotool keydown Control) while the mapped key is down.
 ; Therefore a generic thumb + scroll wheel zoom would not work, as the wheel events are not registered.
 ; To achieve the zoom, we map: mouse thumb button + wheel -> control + +/-. This needs a Guile script.
+; In this config, button 11 is the thumb button, 4 is wheel up and 5 is wheel down.
 
 (define (thumb-binding)
     (xbindkey-function '("b:11") scroll-binding)
