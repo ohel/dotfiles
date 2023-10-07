f9fc43b0284ab970d77dfd69f1f37e6ac84b673a
ohel <ohel@users.noreply.github.com>
2023-10-08 01:44:59 +0300

    Automatically run bluealsa-aplay.

--- scripts/toggle_bluetooth.sh
+++ scripts/toggle_bluetooth.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 # Toggles bluetooth radio. If $1 = power, toggle power instead of rfkill (block toggle).
+# If ~/.config/auto-bluealsa-aplay exists and bluealsa is running, bluealsa-aplay is started/stopped also.
 
 if [ "$1" = "power" ]
 then
     powermode="true"
     [ "$(dbus-send --system --type=method_call --print-reply=literal --dest=org.bluez /org/bluez/hci0 org.freedesktop.DBus.Properties.Get string:org.bluez.Adapter1 string:Powered | grep true)" ] && powermode="false"
     dbus-send --system --type=method_call --print-reply=literal --dest=org.bluez /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:org.bluez.Adapter1 string:Powered variant:boolean:$powermode
-    [ "$(which notify-send 2>/dev/null)" ] && notify-send -h int:transient:1 "Bluetooth power: $powermode" -t 500
+
+    if [ -e ~/.config/auto-bluealsa-aplay ]
+    then
+        [ "$powermode" = "true" ] && ps -e | grep bluealsa && bluealsa-aplay &
+        [ "$powermode" = "false" ] && ps -e | grep bluealsa-aplay && killall bluealsa-aplay
+    fi
+
+    [ "$powermode" = "true" ] && powermode="on" || powermode="off"
+    [ "$(which notify-send 2>/dev/null)" ] && notify-send -h int:transient:1 "Bluetooth: $powermode" -t 500
     exit 0
 fi
 
@@ -14,4 +23,12 @@ cmd=block
 [ $(rfkill list bluetooth | grep yes | wc -l) -gt 0 ] && cmd=unblock
 
 sudo rfkill $cmd bluetooth
+
+if [ -e ~/.config/auto-bluealsa-aplay ]
+then
+    [ "$cmd" = "unblock" ] && ps -e | grep bluealsa && bluealsa-aplay &
+    [ "$cmd" = "block" ] && ps -e | grep bluealsa-aplay && killall bluealsa-aplay
+fi
+
+cmd="$cmd"ed
 [ "$(which notify-send 2>/dev/null)" ] && notify-send -h int:transient:1 "Bluetooth: $cmd" -t 500
