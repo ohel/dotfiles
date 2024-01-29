01ec643804a714f00a98332e5e6b5d7a39b8672b
ohel <ohel@users.noreply.github.com>
2024-01-30 01:51:25 +0200

    Fix X.org max clients; tweak.

--- scripts/matrix_orbital/resetcontrast.sh
+++ scripts/matrix_orbital/resetcontrast.sh
@@ -2,7 +2,7 @@
 
 [ ! -e /dev/serial/matrix_orbital ] && echo No device && exit 1
 
-echo -e "\xFEP\x70" > /dev/serial/matrix_orbital
+echo -e "\xFEP\x80" > /dev/serial/matrix_orbital
 
 # set and save:
-#echo -e "\xFE\x91\x70" > /dev/serial/matrix_orbital
+#echo -e "\xFE\x91\x80" > /dev/serial/matrix_orbital
