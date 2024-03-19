0f3c366de5e3a811c6e76ffa92d46caa297b3616
ohel <ohel@users.noreply.github.com>
2024-03-19 18:59:57 +0200

    Support package.use as directory.

--- scripts/gentoo_configcheck.sh
+++ scripts/gentoo_configcheck.sh
@@ -9,6 +9,17 @@ echo Done checking keywords.
 
 # Using "equery l" we have to use packages without version numbers.
 echo Checking use flags...
-grep -v "^#" /etc/portage/package.use | grep -v "^$" | cut -f 1 -d ' ' | grep -v "^>" | grep -v "^=" | grep -v "^\*" | xargs -I {} sh -c "equery -NC l {} | grep \"No installed\"" equerycmd
-grep -v "^#" /etc/portage/package.use | grep -v "^$" | cut -f 1 -d ' ' | grep "^>\|=" | cut -f 2 -d '=' | sed "s/-[0-9\.r-]*$//" | xargs -I {} sh -c "equery -NC l {} | grep \"No installed\"" equerycmd
+
+if [ -d /etc/portage/package.use ]
+then
+    cd /etc/portage/package.use
+    for usefile in $(ls -1)
+    do
+        grep -v "^#" $usefile | grep -v "^$" | cut -f 1 -d ' ' | grep -v "^>" | grep -v "^=" | grep -v "^\*" | xargs -I {} sh -c "equery -NC l {} | grep \"No installed\"" equerycmd
+        grep -v "^#" $usefile | grep -v "^$" | cut -f 1 -d ' ' | grep "^>\|=" | cut -f 2 -d '=' | sed "s/-[0-9\.r-]*$//" | xargs -I {} sh -c "equery -NC l {} | grep \"No installed\"" equerycmd
+    done
+else
+    grep -v "^#" /etc/portage/package.use | grep -v "^$" | cut -f 1 -d ' ' | grep -v "^>" | grep -v "^=" | grep -v "^\*" | xargs -I {} sh -c "equery -NC l {} | grep \"No installed\"" equerycmd
+    grep -v "^#" /etc/portage/package.use | grep -v "^$" | cut -f 1 -d ' ' | grep "^>\|=" | cut -f 2 -d '=' | sed "s/-[0-9\.r-]*$//" | xargs -I {} sh -c "equery -NC l {} | grep \"No installed\"" equerycmd
+fi
 echo Done checking use flags.
