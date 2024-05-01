585d8990d198c08d992bdff60226a846d53a220d
ohel <ohel@users.noreply.github.com>
2024-05-01 21:33:07 +0300

    Fix Steam flatpak launcher.

--- scripts/launchers/steam.sh
+++ scripts/launchers/steam.sh
@@ -7,7 +7,25 @@ if [ -e ~/.steam ]
 then
     cd ~/.steam/root
     rm -rf config/htmlcache/*
-    ./steam.sh >/tmp/steam.log 2>&1 &
+    ./steam.sh "$@" >/tmp/steam.log 2>&1 &
 else
-    /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=/app/bin/steam --file-forwarding com.valvesoftware.Steam >/tmp/steam.log 2>&1 &
+    # Steam can't handle symlinks in media directories but fails immediately.
+    for link in $(find ~/.var/app/com.valvesoftware.Steam/media/ -type l)
+    do
+        rm $link
+    done
+
+    /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=/app/bin/steam --file-forwarding com.valvesoftware.Steam "$@" >/tmp/steam.log 2>&1 &
+
+    # Steam tries creating the symlinks again and again until it fails, so check and remove if they are being created. Wait 30 seconds for Steam to start.
+    counter=0
+    while [ $counter -lt 30 ]
+    do
+        for link in $(find ~/.var/app/com.valvesoftware.Steam/media/ -type l)
+        do
+            rm $link
+        done
+        sleep 1
+        counter=$(expr $counter + 1)
+    done
 fi
