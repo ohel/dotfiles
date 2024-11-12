a3826f2bd6932fc3f04cae7b78c4635694aeea77
ohel <ohel@users.noreply.github.com>
2024-11-13 00:50:13 +0200

    Support YYYYmmdd_HHMMSS format.

--- scripts/exif_name_to_timestamp.sh
+++ scripts/exif_name_to_timestamp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-# Given filenames of form YYYY.mm.dd.HH.MM.SS.* (where . is any character), parse timestamp and write it as UTC AllDates EXIF info.
+# Given filenames of form YYYY.mm.dd.HH.MM.SS.* or YYYYmmdd_HHMMSS.* (where . is any character), parse timestamp and write it as UTC AllDates EXIF info.
 
 [ ! "$1" ] && echo "Missing arguments." && exit 1
 
@@ -16,14 +16,21 @@ fi
 for inputname in "$@"
 do
     timestamp=$(echo "$inputname" | grep -o "^[0-9]\{4,\}.[0-9][0-9].[0-9][0-9].[0-9][0-9].[0-9][0-9].[0-9][0-9]")
+    [ ! "$timestamp" ] && timestamp=$(echo "$inputname" | grep -o "^[0-9]\{8,\}_[0-9]\{6,\}")
     [ ! "$timestamp" ] && echo "Filename is in wrong form." && continue
 
-    year=$(echo "$timestamp" | sed "s/\(^[0-9]\{4,\}\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\)/\1/")
-    month=$(echo "$timestamp" | sed "s/\(^[0-9]\{4,\}\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\)/\2/")
-    day=$(echo "$timestamp" | sed "s/\(^[0-9]\{4,\}\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\)/\3/")
-    hour=$(echo "$timestamp" | sed "s/\(^[0-9]\{4,\}\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\)/\4/")
-    minute=$(echo "$timestamp" | sed "s/\(^[0-9]\{4,\}\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\)/\5/")
-    second=$(echo "$timestamp" | sed "s/\(^[0-9]\{4,\}\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\)/\6/")
+    if [ $(echo "$timestamp" | grep -o "_") ]
+    then
+        regex="\(^[0-9]\{4,\}\)\([0-9][0-9]\)\([0-9][0-9]\)_\([0-9][0-9]\)\([0-9][0-9]\)\([0-9][0-9]\)"
+    else
+        regex="\(^[0-9]\{4,\}\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9]\)"
+    fi
+    year=$(echo "$timestamp"   | sed "s/$regex/\1/")
+    month=$(echo "$timestamp"  | sed "s/$regex/\2/")
+    day=$(echo "$timestamp"    | sed "s/$regex/\3/")
+    hour=$(echo "$timestamp"   | sed "s/$regex/\4/")
+    minute=$(echo "$timestamp" | sed "s/$regex/\5/")
+    second=$(echo "$timestamp" | sed "s/$regex/\6/")
 
     exiftool -m -overwrite_original -AllDates="$year-$month-$day $hour:$minute:$second UTC" "$inputname"
     [ "$mode" ] && exiftool -m -overwrite_original -AllDates$mode=$offset:0:0 "$inputname"
