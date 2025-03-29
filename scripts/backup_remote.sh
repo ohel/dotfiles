f135c9c183c4aa634ba280cbac9a41fe60df5095
ohel <ohel@users.noreply.github.com>
2025-03-29 04:13:38 +0200

    Add remote root dir parameter.

--- scripts/backup_remote.sh
+++ scripts/backup_remote.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 # A generic echoing backup script to rsync something to or from a remote location.
-# There are four mandatory parameters, the rest are optional rsync excludes.
+# There are five mandatory parameters, the rest are optional rsync excludes.
 # The parameters are:
 # $1: mode (normal, normaldry, reverse, reversedry)
 # $2: localdir (the directory to back up)
 # $3: server (SSH)
-# $4: target (directory in server under $remotedir)
+# $4: remoterootdir (prefix for directories on the remote end, e.g. ~backups)
+# $5: target (directory in server under $remoterootdir)
 # The mode parameter may be postfixed with nowait (e.g. normalnowait) to skip the countdown.
 # By default git working directories are skipped when syncing from a remote location.
 # The mode parameter may be postfixed with gitdirs (e.g. reversegitdirs) to include git directories.
 
-remotedir="~/backups" # The target directory should exist in this directory on the server.
 countdown=3 # Wait this many seconds before actually starting the operation to prevent accidents.
 
 echo
@@ -24,7 +24,8 @@ fi
 mode=$1
 localdir=$2
 server=$3
-target=$4
+remoterootdir=$4
+target=$5
 shift; shift; shift; shift
 excludelist=("$@")
 
@@ -59,20 +60,20 @@ if [ "$mode" == "normal" ]
 then
     echo "Starting \"$target\" backup *TO* remote in $countdown seconds."
     sleep $countdown
-    rsync $rsync_options $excludeparams $localdir $server:$remotedir/$target
+    rsync $rsync_options $excludeparams $localdir $server:$remoterootdir/$target
 elif [ "$mode" == "normaldry" ]
 then
     echo "Dry run backup \"$target\" *TO* remote."
-    rsync -n $rsync_options $excludeparams $localdir $server:$remotedir/$target
+    rsync -n $rsync_options $excludeparams $localdir $server:$remoterootdir/$target
 elif [ "$mode" == "reverse" ]
 then
     echo "Starting \"$target\" synchronizing *FROM* remote in $countdown seconds."
     sleep $countdown
-    rsync $rsync_options $excludeparams $server:$remotedir/$target/ $localdir
+    rsync $rsync_options $excludeparams $server:$remoterootdir/$target/ $localdir
 elif [ "$mode" == "reversedry" ]
 then
     echo "Dry run synchronize \"$target\" *FROM* remote."
-    rsync -n $rsync_options $excludeparams $server:$remotedir/$target/ $localdir
+    rsync -n $rsync_options $excludeparams $server:$remoterootdir/$target/ $localdir
 else
     echo "Unknown backup mode: $mode. Aborted."
 fi
