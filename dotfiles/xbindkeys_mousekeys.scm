7e1ae8ea00494bdfe443f6f4da817c1da3509f3f
ohel <ohel@users.noreply.github.com>
2025-09-27 03:11:57 +0300

    Add wheel tilt.

--- dotfiles/xbindkeys_mousekeys.scm
+++ dotfiles/xbindkeys_mousekeys.scm
@@ -4,6 +4,7 @@
 ; Therefore a generic thumb + scroll wheel zoom would not work, as the wheel events are not registered.
 ; To achieve the zoom, we map: mouse thumb button + wheel -> control + +/-. This needs a Guile script.
 ; In this config, button 11 is the thumb button, 4 is wheel up and 5 is wheel down.
+; Additionally, for tilt wheel or horizontal scroll wheel feature, mouse buttons 6/7 are mapped to send alt+left/right for back and forward.
 
 (define (thumb-binding)
     (xbindkey-function '("b:11") scroll-binding)
@@ -32,6 +33,20 @@
     (xbindkey-function '("b:5")
         (lambda ()
             (run-command "xdotool key ctrl+minus")
+        )
+    )
+
+    ; Scroll left
+    (xbindkey-function '("b:6")
+        (lambda ()
+            (run-command "xdotool key alt+Left")
+        )
+    )
+
+    ; Scroll right
+    (xbindkey-function '("b:7")
+        (lambda ()
+            (run-command "xdotool key alt+Right")
         )
     )
 
