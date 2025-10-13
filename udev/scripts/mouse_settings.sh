5348ca0eb44fd0ecfe722bad24dc800d526c40cc
ohel <ohel@users.noreply.github.com>
2025-10-14 01:57:58 +0300

    Update device id as in new firmware.

--- udev/scripts/mouse_settings.sh
+++ udev/scripts/mouse_settings.sh
@@ -8,12 +8,13 @@ export DISPLAY=:0
 sleep 2
 
 # Button mapping and acceleration settings for Keychron M6, for all three ways of connecting it.
-# Wired.
+
+# Dongle.
 xinput set-button-map "Keychron  Keychron Link-KM" 1 12 3 4 5 6 7 10 11 2>/dev/null
 xinput set-prop "Keychron  Keychron Link-KM" "libinput Accel Speed" 0.25 2>/dev/null
-# Dongle.
-xinput set-button-map "Keychron  Keychron M6 Lite" 1 12 3 4 5 6 7 10 11 2>/dev/null
-xinput set-prop "Keychron  Keychron M6 Lite" "libinput Accel Speed" 0.25 2>/dev/null
+# Wired.
+xinput set-button-map "Keychron Keychron M6" 1 12 3 4 5 6 7 10 11 2>/dev/null
+xinput set-prop "Keychron Keychron M6" "libinput Accel Speed" 0.25 2>/dev/null
 # Bluetooth.
-xinput set-button-map "Keychron M6 Lite Mouse" 1 12 3 4 5 6 7 10 11 2>/dev/null
-xinput set-prop "Keychron M6 Lite Mouse" "libinput Accel Speed" 0.25 2>/dev/null
+xinput set-button-map "Keychron M6 Mouse" 1 12 3 4 5 6 7 10 11 2>/dev/null
+xinput set-prop "Keychron M6 Mouse" "libinput Accel Speed" 0.25 2>/dev/null
