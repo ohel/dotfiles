a37579cd69447e422f81972829768fc7502ab53e
ohel <ohel@users.noreply.github.com>
2025-10-19 16:46:18 +0300

    Update paths and tidy up.

--- scripts/launchers/local_llm.sh
+++ scripts/launchers/local_llm.sh
@@ -3,8 +3,8 @@
 # $1 = stop|ollama. If "stop", stop serving and kill Open WebUI. If "ollama", Open WebUI is not started.
 # $2 = context size for Ollama, e.g. 8192 for Aider. Optional.
 
-prefix=/opt/programs/ollama
-openwebui=/opt/programs/open-webui-venv
+ollama_path=/opt/ai_tools/ollama
+openwebui_path=/opt/ai_tools/open-webui-venv
 logsdir=~/.cache/local_llm_logs
 scriptsdir=$(dirname "$(readlink -f "$0")")
 
@@ -12,9 +12,9 @@ mkdir -p $logsdir
 
 if [ "$1" = "stop" ]
 then
-    pid=$(ps -ef | grep "$prefix/bin/ollama serve$" | tr -s ' ' | cut -f 2 -d ' ')
+    pid=$(ps -ef | grep "$ollama_path/bin/ollama serve$" | tr -s ' ' | cut -f 2 -d ' ')
     [ "$pid" ] && kill $pid
-    pid=$(ps -ef | grep "$openwebui/bin/open-webui serve$" | tr -s ' ' | cut -f 2 -d ' ')
+    pid=$(ps -ef | grep "$openwebui_path/bin/open-webui serve$" | tr -s ' ' | cut -f 2 -d ' ')
     [ "$pid" ] && kill $pid
     exit 0
 fi
@@ -22,21 +22,21 @@ fi
 ctx=""
 [ "$2" ] && ctx="OLLAMA_CONTEXT_LENGTH=$2"
 
-if ! ps -ef | grep -q "$prefix/bin/ollama serve$"
+if ! ps -ef | grep -q "$ollama_path/bin/ollama serve$"
 then
-    env $ctx LD_LIBRARY_PATH=$prefix/lib $prefix/bin/ollama serve > $logsdir/ollama.log 2>&1 &
+    env $ctx LD_LIBRARY_PATH=$ollama_path/lib $ollama_path/bin/ollama serve > $logsdir/ollama.log 2>&1 &
 else
     echo "Ollama already running."
 fi
 
 [ "$1" = "ollama" ] && exit 0
 
-if ! ps -ef | grep "$openwebui/bin/open-webui serve$"
+if ! ps -ef | grep "$openwebui_path/bin/open-webui serve$"
 then
-    . $openwebui/bin/activate
+    . $openwebui_path/bin/activate
     # cd so that .webui_secret_key is read from ~
     cd ~
-    open-webui serve > $logsdir/openwebui.log 2>&1 &
+    $openwebui_path/bin/open-webui serve > $logsdir/openwebui.log 2>&1 &
     sleep 5
 fi
 
