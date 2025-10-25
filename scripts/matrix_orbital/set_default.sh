d2ac170fa33be2911a91fab1792eaaf4f982f039
ohel <ohel@users.noreply.github.com>
2025-10-25 18:27:44 +0300

    Add LK202-25-USB device.

--- scripts/matrix_orbital/set_default.sh
+++ scripts/matrix_orbital/set_default.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/sh
 dev=/dev/serial/matrix_orbital
 
-printf "|\xcd/|||\xcd ||/\x60 | ||\xcd ||  ||| \xcd||\xcd_\xba|_|| \xcd|" > $dev
+# LK202-24-USB font
+#printf "|\xcd/|||\xcd ||/\x60 | ||\xcd ||  ||| \xcd||\xcd_\xba|_|| \xcd|" > $dev
+# LK202-25-USB font
+printf "|\x5c/|||\x5c ||/\xbf\xb3| ||\x5c ||  ||| \x5c||\x5c_\x5d|_|| \x5c|" > $dev
 
 # set default screen
-printf "\xFE@|\xcd/|||\xcd ||/\x60 | ||\xcd ||  ||| \xcd||\xcd_\xba|_|| \xcd|" > $dev
+printf "\xFE@|\x5c/|||\x5c ||/\xbf\xb3| ||\x5c ||  ||| \x5c||\x5c_\x5d|_|| \x5c|" > $dev
